52ecec06a227d26042a6eb3b8d85cf710f6f0377
Hong Seung-Gi <tmdrl5661@naver.com>
2022-04-20 17:20:38 +0900

    #S06P31A203-68 [Add] 메인화면 구성 진행

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styles from './style/Main.module.scss';
 
 
 function Main() {
+    const [open, setOpen] = useState<boolean>(false);
+    
+    const handleOpenCreateRoom = (e : React.MouseEvent<HTMLButtonElement>) =>{
+        e.preventDefault();
+        setOpen(true);       
+    }
+    const handleCloseCreateRoom = (e : React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setOpen(false);
+    }
     return (
         <div className={styles.main}>
             <div className={styles.glass}>
-                <p className={styles.title}>Arcade</p>
-                <button className={styles.button}>방 만들기</button>
+                <p className={styles.glitch} data-text="Arcade">Arcade</p>
+                <button className={styles.button} onClick={handleOpenCreateRoom}>방 만들기</button>
                 <button className={styles.button}>입장하기</button>
             </div>
         </div>
