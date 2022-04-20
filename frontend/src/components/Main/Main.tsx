1b14a06653c4a8127642d34ff70f7fb73ab12ff9
Hong Seung-Gi <tmdrl5661@naver.com>
2022-04-20 16:09:50 +0900

    #S06P31A203-68 [Add] 메인화면 구성 진행중

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
+import styles from './style/Main.module.scss';
 
 
 function Main() {
-    return (<>하이하이</>);
+    return (
+        <div className={styles.main}>
+            <div className={styles.glass}>
+                <p className={styles.title}>Arcade</p>
+                <button className={styles.button}>방 만들기</button>
+                <button className={styles.button}>입장하기</button>
+            </div>
+        </div>
+    );
 }
 
 export default Main;
