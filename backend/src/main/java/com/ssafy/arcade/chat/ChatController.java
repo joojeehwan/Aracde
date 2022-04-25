f89e1fd43075fc25e215834d63f4a4567ab861f4
qweadzs <qweadzs@naver.com>
2022-04-25 15:17:17 +0900

    #S06P31A203-62 [feat] WebSocketConfig 설정

--- backend/src/main/java/com/ssafy/arcade/chat/ChatController.java
+++ backend/src/main/java/com/ssafy/arcade/chat/ChatController.java
@@ -1,25 +1,32 @@
 package com.ssafy.arcade.chat;
 
+import com.ssafy.arcade.chat.dtos.ChatMessageDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final SimpMessageSendingOperations simpMessageSendingOperations;
+    private final SimpMessagingTemplate template; //특정 Broker로 메세지를 전달
 
-    /*
-        /sub/channel/12345      - 구독(channelId:12345)
-        /pub/hello              - 메시지 발행
-    */
+    //Client가 SEND할 수 있는 경로
+    //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
+    //"/pub/chat/enter"
+    @MessageMapping(value = "/chat/enter")
+    public void enter(ChatMessageDTO message) {
+        message.setMessage(message.getWriter() + "님이 채팅방에 참여하였습니다.");
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
 
-    @MessageMapping("/hello")
-    public void message(Message message) {
-
-
-        simpMessageSendingOperations.convertAndSend("/sub/channel/" + message.getChannelId(), message);
+    @MessageMapping(value = "/chat/message")
+    public void message(ChatMessageDTO message) {
+        // 메시지가 들어오면 /sub/chat/room/룸아이디 로 메시지를 보낸다.
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
