6d5736bf7368c87971576c1f6502a734d2e17f3b
DESKTOP-KVCQHCD\SSAFY <amlwq@naver.com>
2022-05-02 16:05:26 +0900

    [Fix] 배포 스크립트 수정...

--- deploy.sh
+++ deploy.sh
@@ -1,68 +1,98 @@
 # jenkins의 기존 경로는 /var/lib/jenkins/workspace/arcade 이다.
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++백엔드 properties 복사하기"
 cp /home/ubuntu/properties/*.properties /var/lib/jenkins/workspace/arcade/backend/src/main/resources/
+echo "+++++++++++++++++++++++++++++++++++"
 
 # --- 백엔드 ----
 
 # 기존 백엔드 컨테이너 멈추고 지우기
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++백엔드 docker 컨테이너 멈추고 지우기"
 docker stop arcade_backend
 docker rm arcade_backend
+echo "+++++++++++++++++++++++++++++++++++"
 
 # 기존 백엔드 이미지 지우기
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++백엔드 도커 이미지 지우기"
 docker rmi arcade_backend
+echo "+++++++++++++++++++++++++++++++++++"
 
 # 새로운 백엔드 이미지 만들기
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++백엔드 도커 이미지 새로 만들기"
 pwd
 docker build -t arcade_backend backend
+echo "+++++++++++++++++++++++++++++++++++"
 # 새로운 백엔드 컨테이너 실행하기
 echo "++++++++++백엔드 도커 새로만든 이미지로 컨테이너 실행하기"
 docker run -d --name arcade_backend -p 8080:8080 arcade_backend
+echo "+++++++++++++++++++++++++++++++++++"
 
 # ---- 프론트엔드 ----
 # 1. frontend 폴더로 이동
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++프론트엔드 폴더로 이동"
 cd frontend
+echo "+++++++++++++++++++++++++++++++++++"
 # 2. npm install & npm run build
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++노드랑 npm 버전 확인"
 node -v
 npm -v
-
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++npm i & npm run build"
 npm i
 npm run build
+echo "+++++++++++++++++++++++++++++++++++"
 # 3. 기존에 있던 폴더를 지운다.
 echo "++++++++++기존의 opt/openvidu 에 있는 프론트 빌드 파일 제거"
 
 sudo rm -f /opt/openvidu/front_build/index.html
 sudo rm -rf /opt/openvidu/front_build/assets
+echo "+++++++++++++++++++++++++++++++++++"
 # 4. 빌드한 결과물을 복사한다.
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++프론트 빌드한 결과물 복사"
-sudo cp -r dist/index.html /opt/openvidu/front_build
+sudo cp dist/index.html /opt/openvidu/front_build
 sudo cp -r dist/assets /opt/openvidu/front_build
-
+echo "+++++++++++++++++++++++++++++++++++"
 
 # --- openvidu ----
 # 1. home/ubuntu/openvidu/openvidu-server 폴더 제거(갈아끼울거임)
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++home/ubuntu/openvidu/openvidu-server 폴더 제거(갈아끼울거임)"
-rm -rf /home/ubuntu/openvidu/openvidu-server
+sudo rm -rf /home/ubuntu/openvidu/openvidu-server
+echo "+++++++++++++++++++++++++++++++++++"
 # 2. openvidu-server 폴더 복붙
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++openvidu-server 폴더 복붙"
-cp -r openvidu-server /home/ubuntu/openvidu/openvidu-server
+cp -r ../openvidu-server /home/ubuntu/openvidu/openvidu-server
+echo ""+++++++++++++++++++++++++++++++++++
 # 3. 메이븐 빌드
-echo "++++++++++/home/ubuntu/openvidu/openvidu-server 메이븐 빌드중"
+echo "+++++++++++++++++++++++++++++++++++"
+echo "++++++++++/home/ubuntu/openvidu/openvidu-server로 이동해서 메이븐 빌드"
 cd /home/ubuntu/openvidu/openvidu-server
+echo "++++++++++권한 문제. ubnutu 계정으로 바꿔봄"
+su ubnutu
+echo "++++++++++권한 문제. 혹시 모르니까 target 폴더 권한 받아옴"
+sudo chmod -R 777 target
 mvn install -DskipTests
+echo "+++++++++++++++++++++++++++++++++++"
 # 4. 도커경로로 이동하기
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++openvidu-server 도커 폴더로 이동"
 cd docker/openvidu-server
+echo "+++++++++++++++++++++++++++++++++++"
 # 5. openvidu-server 이미지 빌드
+echo "+++++++++++++++++++++++++++++++++++"
 echo "++++++++++openvidu-server 이미지 빌드하기"
 chmod +x create_image.sh
+echo "++++++++++혹시 모르니까 root계정으로 변경해봄"
+sudo su
 ./create_image.sh arcade
-
+echo "+++++++++++++++++++++++++++++++++++"
 
 # --- openvidu restart
 # cd /opt/openvidu
