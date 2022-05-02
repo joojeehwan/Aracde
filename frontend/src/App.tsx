bc134c59c55f3474bf6487f8ea4d8ae38859bea6
kimms4142 <kimms4142@naver.com>
2022-05-02 10:37:12 +0900

    [Fix] 주석제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,8 +21,8 @@ function App() {
 
           <Route path='/' element={<Main />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path='/myroom' element={<MyRoom />} /> */}
-          {/* <Route path='/entrance' element={<EntranceRoom />} /> */}
+          <Route path='/myroom' element={<MyRoom />} />
+          <Route path='/entrance' element={<EntranceRoom />} />
           <Route path="/oauth/callback/kakao" element={<KakaoRedirectHandler />} />
           <Route path="/oauth/callback/naver" element={<NaverRedirectHandler />} />
           <Route path="/oauth/callback/google" element={<GoogleRedirectHandler />} />
