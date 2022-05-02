6ecea2b622e7db2697395bb079e1d14f64306db5
Hong Seung-Gi <tmdrl5661@naver.com>
2022-05-02 21:55:37 +0900

    [Add] vite 프로젝트 삭제

--- frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-const svgrPlugin = require('vite-plugin-svgr');
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgrPlugin({ svgrOptions: { icon: true } }), react()],
-});
