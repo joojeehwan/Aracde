26b217d3689aef39421805ca61c928818c86819d
Hong Seung-Gi <tmdrl5661@naver.com>
2022-05-05 15:23:15 +0900

    [Add] 브랜치 초기화

--- frontend/src/components/Login/naver/OAuth.ts
+++
@@ -1,5 +0,0 @@
-const CLIENT_ID = 'Jb6yH7fmEzBhClzMoClq';
-const REDIRECT_URI = 'http://localhost:3000/oauth/callback/naver';
-const CLIENT_SECRET = '086J2INDmq';
-
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&state=arcade&redirect_uri=${REDIRECT_URI}`;
--- frontend/src/components/Store/info.ts
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-export const infoStore = create<{nickname : string, invitecode : string, setNick : (payload : string) => void, setInviteCode : (payload : string) => void}>(set => ({
-    nickname : "",
-    invitecode : "",
-    setNick : (payload : string) => set(state => ({nickname : payload})),
-    setInviteCode : (payload : string ) => set(state => ({invitecode : payload}))
-}))
