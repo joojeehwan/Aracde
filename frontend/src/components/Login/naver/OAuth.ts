0045d26ec3d29e8736729cc906282e2bff222feb
jeehwan <meanstrike94@gmail.com>
2022-05-09 11:08:32 +0900

    .env 소셜로그인 클라이언트 ID

--- frontend/src/components/Login/naver/OAuth.ts
+++ frontend/src/components/Login/naver/OAuth.ts
@@ -1,4 +1,4 @@
-const CLIENT_ID = 'Jb6yH7fmEzBhClzMoClq';
+const CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID;
 const REDIRECT_URI = process.env.REACT_APP_API_ROOT_NOTV1 + '/oauth/callback/naver';
 
 export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&state=arcade&redirect_uri=${REDIRECT_URI}`;
