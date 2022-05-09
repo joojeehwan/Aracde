8a71b74b33714ed7a0b6361a4fff23aac86cd934
jeehwan <meanstrike94@gmail.com>
2022-05-09 11:11:03 +0900

    .env파일 소셜로그인 부분 수정

--- frontend/src/components/Login/kakao/OAuth.ts
+++ frontend/src/components/Login/kakao/OAuth.ts
@@ -1,4 +1,4 @@
-const CLIENT_ID = 'b72153259a249f6c1e50dd7fb421627a';
+const CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
 const REDIRECT_URI = process.env.REACT_APP_API_ROOT_NOTV1 + '/oauth/callback/kakao';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
