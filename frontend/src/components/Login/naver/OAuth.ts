4fc3e817e5d1c64f7ec6f0f937e3b150cf758ff4
jeehwan <meanstrike94@gmail.com>
2022-05-13 09:49:05 +0900

    "frontend Naver Oauth수정"

--- frontend/src/components/Login/naver/OAuth.ts
+++ frontend/src/components/Login/naver/OAuth.ts
@@ -1,8 +1,4 @@
-// const CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID;
-// const REDIRECT_URI = process.env.REACT_APP_API_ROOT_NOTV1 + '/oauth/callback/naver';
+const CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID;
+const REDIRECT_URI = process.env.REACT_APP_API_ROOT_NOTV1 + '/oauth/callback/naver';
 
-// export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&state=arcade&redirect_uri=${REDIRECT_URI}`;
-
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=Jb6yH7fmEzBhClzMoClq&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2Fcallback%2Fnaver&state=hDGK1FAFK9FSDF2551dfDGd5s7s2sJ49SSsf2tSFG`;
-
-
+export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&state=arcade&redirect_uri=${REDIRECT_URI}`;
